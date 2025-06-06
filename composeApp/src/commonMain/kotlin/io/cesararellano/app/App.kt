5bcd773c393ddf706c3dbd2edecaf883b5eb96e2
Cesar Arellano <raywayday@gmail.com>
2025-06-06 00:20:32 -0600

    first commit

---
+++ composeApp/src/commonMain/kotlin/io/cesararellano/app/App.kt
@@ -0,0 +1,43 @@
+package io.cesararellano.app
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Box(Modifier.statusBarsPadding().background(Color.White)) {
+            MyButton("Click Me!")
+        }
+
+    }
+}
+
+@Composable
+fun MyButton(text: String) {
+    Text(
+        text = text,
+        modifier = Modifier
+            .background(Color.Magenta, shape = RoundedCornerShape(8.dp))
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .clickable { println("Clicked") }
+    )
+}
